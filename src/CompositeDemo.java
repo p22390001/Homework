55923b2e618fed3153f5ee111b70e944bdbbe90c
Chen Guan-Lin <p22390001@gmail.com>
2015-12-21 23:53:03 +0800

    add

--- src/CompositeDemo.java
+++ src/CompositeDemo.java
@@ -2,7 +2,24 @@
 public class CompositeDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Paper Paper1=new Paper(),Paper2=new Paper();
+		
+		Paper1.AddQuestion(new Question("5X9=?"));
+		Paper1.AddQuestion(new Question("9+100=?"));
+		
+		Paper2.AddQuestion(Paper1.GetQuestion(1));
+		Paper2.AddQuestion(new Question("100X100=?"));
+		Paper2.AddQuestion(new Question("50-36=?"));
+		
+		System.out.println("考卷1:");
+		Paper1.print();
+		
+		System.out.println("考卷2:");
+		Paper2.print();
+		
+		Paper2.RemoveQuestion(Paper2.GetQuestion(2));
+		System.out.println("考卷2(移除第2題):");
+		Paper2.print();
 
 	}
 
